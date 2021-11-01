021c323fe2145bc1c7dd5bfd3a208b915a67de43
Juan Carlos Castillo Cano <jccastillocano@gmail.com>
2021-11-01 09:22:11 +0000

    Fix latest bits for envs and creds

--- crmservice/settings.py
+++ crmservice/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_jagtrm4vbfezv(cnbz&ek+ig5rno3c&+^s38m6b@t-9+%e)xg'
+SECRET_KEY = os.getenv('SECRET_KEY', 'xxx')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
