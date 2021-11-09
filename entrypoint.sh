dde906fd226151fcb3014c3b924ad8d66e042e01
Juan Carlos <jccastillocano@gmail.com>
2021-11-09 23:07:19 +0000

    HOTFIX: validate password (#6)
    * Validate passwords
    
    * Fix validate password

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function wait_for_db()
+{
+  while ! ./manage.py sqlflush > /dev/null 2>&1 ;do
+    echo "Waiting for the db to be ready."
+    sleep 1
+  done
+}
+
+wait_for_db
+python manage.py runserver 0.0.0.0:8000
